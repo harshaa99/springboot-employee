037b1e41c78640ed81ad178d71286da0239b9e86
harshaa99 <60323622+harshaa99@users.noreply.github.com>
2020-02-12 10:00:49 +0000

    Crated new project

---
+++ EmployeeApplication/src/test/java/com/company/employee/co/uk/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.employee.co.uk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
