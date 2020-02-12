7cc346f60055504abff59633fa1c8ee8e8847193
harshaa99 <60323622+harshaa99@users.noreply.github.com>
2020-02-12 10:46:09 +0000

    Updated DAO methods

--- EmployeeApplication/src/main/java/com/company/employee/co/uk/repo/EmployeeRepo.java
+++ EmployeeApplication/src/main/java/com/company/employee/co/uk/repo/EmployeeRepo.java
@@ -1,12 +1,48 @@
 /**
  * 
  */
-package com.company.employee.co.uk.repo;
+package com.company.employee.co.uk.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.company.employee.co.uk.model.Employee;
+import com.company.employee.co.uk.repo.EmployeeRepo;
 
 /**
- * @author Sriharshaa
+ * @author Damodar
  *
  */
-public class EmployeeRepo {
-
+@Service
+public class EmployeeDAO {
+	@Autowired
+	EmployeeRepo empRepository;
+	
+	//Creating employee records
+	public List<Employee> saveEmployeeDetails(List<Employee> empDetails){
+		
+		return empRepository.saveAll(empDetails);
+	}
+	
+	//Getting all employee details
+	public List<Employee> getEmployeeDetails(){
+		return empRepository.findAll();
+	}
+	
+	// Get an employee
+	public Employee getEmpDetails(Integer id) {
+		return empRepository.getOne(id);
+	}
+	
+	//updating an employee with location
+	public Employee updateEmployee(Integer empID){
+		return empRepository.getOne(empID);
+	}
+	
+	//Deleting an employee
+	public void deletEmployee(Employee employee) {
+		empRepository.delete(employee);
+	}
 }
